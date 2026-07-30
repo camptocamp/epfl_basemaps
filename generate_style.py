28c3a71e99d6d8791987439681acf174af3130eb
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-07-30 09:27:42 +0200

    fix-epfl-parking-color

--- generate_style.py
+++ generate_style.py
@@ -2018,10 +2018,8 @@ namedstyles = {
       'footway_clr': '"#000000"'
    },
    'epfl': {
-      #'service_clr': '"#f7efb7"',
-      #'service_ol_clr': '"#f2e799"',
-      'parking_clr': '"#eeeeee"',
-      'parking_ol_clr': '"#eeeeee"',
+      'parking_clr': '"#f7efb7"',
+      'parking_ol_clr': '"#eeeeb4"',
    },
 }
 
